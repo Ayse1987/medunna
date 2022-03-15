f132523252f45cf6ed9fdbe5aac02be90710ea60
Ayse Kaya <kayayse1987@gmail.com>
2022-03-15 17:02:21 +0000

    new feature

---
+++ src/test/java/stepdefinitions/uiStepDefs/Interview.java
@@ -0,0 +1,20 @@
+package stepdefinitions.uiStepDefs;
+
+public class Interview {
+
+    public static void main(String[] args) {
+        String name="Ayse Ayse Ayse";
+    }
+
+    public void letter(String str){
+        String actual="";
+        for(int i=0;i<str.length();i++){
+           // if(!actual.(str.charAt(i))){
+                actual+=str.charAt(i);
+
+            }
+
+
+
+    }
+}
